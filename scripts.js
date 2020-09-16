55ecbcfb9fb5411f2d8b1bffe4aea33e9b80f8e6
Daibel Inle Martíne Sánche- Konrad Zuse <konradzuse.dm@gmail.com>
2020-09-16 09:28:17 -0600

    Working on scripts.js -- creating an objet and changing properties

--- scripts.js
+++ scripts.js
@@ -6,10 +6,24 @@ function sayHello(){
         console.log(i+m)  ;
         
     } 
-    console.log(i)  // here blow an error because of scope 
-    console.log(m); // here blow an error because of scope 
+    // ---- Commented to erase these errors ----
+   // console.log(i)  // here blow an error because of scope 
+   // console.log(m); // here blow an error because of scope 
 }
 
 
 // call the function
 sayHello();
+
+
+// create a person object with twoe methods
+ const person = {
+     name:'Konrad',
+     talk(){},
+     walk(){}
+ }
+ 
+ person.name = 'Pedro';
+ person.talk();
+ let targetProperty = 'name';
+ person[targetProperty] = 'Pablo'; // It worked fine but I dislike it
